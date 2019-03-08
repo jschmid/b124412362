0165c70ab3d69fed4ad8322d14bc291cb45c49d4
Jonas Schmid <jonas.schmid@gmail.com>
2019-03-08 10:09:58 +0100

    Workaround for ViewPager

--- app/src/main/java/pro/schmid/bug124412362/MainActivity.kt
+++ app/src/main/java/pro/schmid/bug124412362/MainActivity.kt
@@ -1,7 +1,9 @@
 package pro.schmid.bug124412362
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -20,5 +22,20 @@ class MainActivity : AppCompatActivity() {
         for (i in 1..20) { list.add("Hello ${Random.nextInt()}")}
 
         pagesAdapter.items = list
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        // This is a hack while this bug is fixed: https://issuetracker.google.com/issues/124412362
+        // We reset the ViewPager's orientation to the wanted one
+        if (savedInstanceState != null) {
+            viewPager.orientation = when (resources.configuration.orientation) {
+                Configuration.ORIENTATION_PORTRAIT -> ViewPager2.ORIENTATION_HORIZONTAL
+                Configuration.ORIENTATION_LANDSCAPE -> ViewPager2.ORIENTATION_VERTICAL
+                else -> ViewPager2.ORIENTATION_HORIZONTAL
+            }
+        }
+
     }
 }

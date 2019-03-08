6ae9b4bde8cf280cfa7c168cedb55cef8ea93653
Jonas Schmid <jonas.schmid@gmail.com>
2019-03-08 08:44:21 +0100

    Initial commit

---
+++ app/src/main/java/pro/schmid/bug124412362/MainActivity.kt
@@ -0,0 +1,12 @@
+package pro.schmid.bug124412362
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
